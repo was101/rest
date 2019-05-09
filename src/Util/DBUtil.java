23cbd50c34834fe5b6281157169eed35c290bf43
whtjd <whtjd@DESKTOP-V2PLDD2>
2019-05-09 19:58:14 +0900

    DBUtil 안됬쪙 ㅠㅠ

--- src/Util/DBUtil.java
+++ src/Util/DBUtil.java
@@ -8,9 +8,9 @@ public class DBUtil {
 		Connection conn = null;
 		
 		
-		String url = "jdbc:mysql://eungho77.ipdisk.co.kr:3306/rest";
+		String url = "jdbc:mysql://eungho77.ipdisk.co.kr:3306/rest?CharacterEncording=UTF-8&serverTimezone=UTC&useSSL=false";
 		String id = "keh_as";
-		String pass = "";
+		String pass = "personal1234!@";
 		try {
 			conn = DriverManager.getConnection(url, id, pass);
 		} catch (SQLException e) {
